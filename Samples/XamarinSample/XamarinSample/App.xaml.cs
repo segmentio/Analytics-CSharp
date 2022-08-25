1b621d0e886661e7096ce0f32ff27def61b218bc
Wenxi Zeng <wenxi.zeng@segment.com>
2022-08-25 12:34:27 -0700

    add xamarin sample

---
+++ Samples/XamarinSample/XamarinSample/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Segment.Analytics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace XamarinSample
+{
+    public partial class App : Application
+    {
+        public static Analytics analytics { get; private set; }
+        
+        public App()
+        {
+            InitializeComponent();
+
+            var configuration = new Configuration("HhvdP2KyRHb1XGiSTHjBcJiHckP6efii",
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            analytics = new Analytics(configuration);
+            
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            analytics.Track("Application Opened");
+        }
+
+        protected override void OnSleep()
+        {
+            analytics.Track("Application Backgrounded");
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
