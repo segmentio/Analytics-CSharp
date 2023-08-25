e5ce292a28f807af4197a7f30d4590a20bfe715e
Wenxi Zeng <wenxi.zeng@segment.com>
2023-08-25 13:59:55 -0700

    add unit test

---
+++ Tests/Utilities/LoggingTest.cs
@@ -0,0 +1,56 @@
+using System;
+using Moq;
+using Segment.Analytics;
+using Segment.Analytics.Utilities;
+using Segment.Serialization;
+using Segment.Sovran;
+using Xunit;
+
+namespace Tests.Utilities
+{
+    public class LoggingTest
+    {
+
+        private readonly Store _store;
+
+        private Settings _settings;
+
+        private readonly Configuration _configuration;
+
+        private readonly Mock<IStorage> _storage;
+
+        public LoggingTest()
+        {
+            _store = new Store(true);
+            _settings = new Settings
+            {
+                Integrations = new JsonObject
+                {
+                    ["foo"] = "bar"
+                }
+            };
+            _configuration = new Configuration(
+                writeKey: "123",
+                autoAddSegmentDestination: false,
+                userSynchronizeDispatcher: true,
+                defaultSettings: _settings
+            );
+            _storage = new Mock<IStorage>();
+        }
+
+        [Fact]
+        public void TestLog()
+        {
+            var logger = new Mock<ILoggerCallback>();
+            Analytics.Logger = logger.Object;
+            var exception = new Exception();
+            _storage
+                .Setup(o => o.Read(It.IsAny<StorageConstants>()))
+                .Throws(exception);
+
+            Segment.Analytics.System.DefaultState(_configuration, _storage.Object);
+
+            logger.Verify(o => o.Log(LogLevel.Error, exception, It.IsAny<string>()), Times.Exactly(1));
+        }
+    }
+}
