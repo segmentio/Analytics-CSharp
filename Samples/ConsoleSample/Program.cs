8ff99cd7b28c55c360c9a48e0d23d50ca28be5ca
Wenxi Zeng <wenxi.zeng@segment.com>
2023-08-28 09:59:53 -0700

    add logger example

--- Samples/ConsoleSample/Program.cs
+++ Samples/ConsoleSample/Program.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Threading;
 using Segment.Analytics;
+using Segment.Analytics.Utilities;
 using Segment.Concurrent;
 
 
@@ -11,6 +12,7 @@ var configuration = new Configuration("YOUR WRITE KEY",
     flushInterval: 10,
     exceptionHandler: new ErrorHandler());
 var analytics = new Analytics(configuration);
+Analytics.Logger = new SegmentLogger();
 
 analytics.Identify("foo");
 analytics.Track("track right after identify");
@@ -23,5 +25,29 @@ class ErrorHandler : ICoroutineExceptionHandler
     public void OnExceptionThrown(Exception e)
     {
         Console.WriteLine(e.StackTrace);
+    }
+}
+
+class SegmentLogger : ISegmentLogger
+{
+    public void Log(LogLevel logLevel, Exception exception = null, string message = null)
+    {
+        switch (logLevel)
+        {
+            case LogLevel.Warning:
+            case LogLevel.Information:
+            case LogLevel.Debug:
+                Console.Out.WriteLine("Message: " + message);
+                break;
+            case LogLevel.Critical:
+            case LogLevel.Trace:
+            case LogLevel.Error:
+                Console.Error.WriteLine("Exception: " + exception?.StackTrace);
+                Console.Error.WriteLine("Message: " + message);
+                break;
+            case LogLevel.None:
+            default:
+                break;
+        }
     }
 }
